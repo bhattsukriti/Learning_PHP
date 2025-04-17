d8fdc5f4022779029ce505142aa45af99de5c596
Sukriti Bhatt <159408248+bhattsukriti@users.noreply.github.com>
2025-04-17 11:55:34 +0530

    Create data and variable

---
+++ data and variable.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Scaler type -->
+     <?php  
+     $strings = " Hello World";
+     echo $strings;
+     $float = 6.4534;
+     echo $float;
+     $bool = True;
+     echo $bool;
+     $int = 123445;
+     echo $int;
+     
+
+    //  <!-- Array Types --->
+     $name = ["Sukriti", "Sahil", "Sonam"];
+
+    //  Objects
+    // We have a object that is equal to the variable. These are created by classes
+    // $object = car(); 
+    ?>
+</body>
+</html>
